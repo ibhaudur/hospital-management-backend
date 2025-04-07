1745484c3e6f548fafe7e7e80f6decb6350efa0a
DevVishal99 <vishalbalekundri88@gmail.com>
2025-04-07 16:14:50 +0530

    Intial Setup

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import connectDB from "./config/db.config";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import logger from "./utils/logger";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({ message: "Hiii" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`connected to port ${PORT}`);
+  logger.info("Database Connected!");
+});
